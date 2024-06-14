f54ffb4e0ce7e027c28472d47e0ae94bf9c37f9d
Dylan Cespedes <Dylancesp001@hotmail.com>
2024-06-14 14:55:17 -0300

    commit 14 del 6

---
+++ testFormParcial/AlmacenDeComponentes.cs
@@ -0,0 +1,206 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testFormParcial
+{
+
+    public partial class AlmacenDeComponentes : Form
+    {
+        private List<Usuario> usuario;
+
+        public AlmacenDeComponentes()
+        {
+            InitializeComponent();
+
+
+
+            usuario = new List<Usuario>();
+
+            string FechaActual = DateTime.Now.ToString("yyyy-MM-dd");
+
+            label3.Text = FechaActual;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var btnAgregar = new AgregarComponente();
+            btnAgregar.Enabled = true;
+
+            DialogResult dialogResult = btnAgregar.ShowDialog();
+
+
+            if (dialogResult == DialogResult.OK) 
+            {
+
+                Componente componente = btnAgregar.Componente;
+
+
+                //Hacer lo mismo con tarjeta grafica y con memoriaRAM
+                if(componente is DiscoDuro)
+                {
+                    DiscoDuro discoDuro = (DiscoDuro)componente;
+                    Administracion.componentes.Add(discoDuro);
+                }
+
+                if(componente is MemoriaRAM)
+                {
+                    MemoriaRAM memoriaRAM = (MemoriaRAM)componente;
+                    Administracion.componentes.Add(memoriaRAM);
+                }
+
+                if(componente is TarjetaGrafica)
+                {
+                    TarjetaGrafica tarjetaGrafica = (TarjetaGrafica)componente;
+                    Administracion.componentes.Add(tarjetaGrafica);
+                }
+
+                JsonComponentes serializer = new JsonComponentes("../../../../Componentes.json");
+                serializer.GuardarComponentesJson(Administracion.componentes);
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = Administracion.componentes;
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            Componente componenteModificar;
+            Componente componenteEncontrado;
+
+            if (this.dataGridView1.SelectedRows.Count > 0) 
+            {
+                Epago metodoPago;
+                EtipoEntrega tipoEntrega;
+                
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+
+                string nombre = dataGridView1.SelectedRows[0].Cells["nombre"].Value.ToString();
+                DateTime fechaEntrega = Convert.ToDateTime(dataGridView1.SelectedRows[0].Cells["fechaEntrega"].Value);
+                int precio = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["precio"].Value);
+                
+                int numeroDeTransferencia = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["numeroDeTransferencia"].Value);
+
+                var columnaMetodoPago = dataGridView1.SelectedRows[0].Cells["metodoPago"].Value;
+                if (columnaMetodoPago != null)
+                {
+                    int intValue;
+                    if (int.TryParse(columnaMetodoPago.ToString(), out intValue))
+                    {
+                        metodoPago = (Epago)intValue;
+                    }
+                }
+
+                var columnatipoEntrega = dataGridView1.SelectedRows[0].Cells["tipoEntrega"].Value;
+                if (columnatipoEntrega != null)
+                {
+                    int intValue;
+                    if (int.TryParse(columnatipoEntrega.ToString(), out intValue))
+                    {
+                        tipoEntrega = (EtipoEntrega)intValue;
+                    }
+                }
+
+                componenteEncontrado = Administracion.BuscarComponente(id);
+
+
+                //Tengo que recuperar ya sea un disco duro, memoria ram o tarjeta grafica
+
+
+
+                //Crear un modificar componente y le vamos a mandar los datos del componente a modificar y luego esos datos hay que cargarlos dentro de los textbox
+
+                modificarComponente modificarComponente = new modificarComponente(componenteEncontrado);
+                modificarComponente.ShowDialog();
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = Administracion.componentes;
+
+            }
+
+        }
+        
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.SelectedRows.Count > 0)
+            {
+                Epago metodoPago;
+                EtipoEntrega tipoEntrega;
+                
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                string nombre = dataGridView1.SelectedRows[0].Cells["nombre"].Value.ToString();
+                DateTime fechaEntrega = Convert.ToDateTime(dataGridView1.SelectedRows[0].Cells["fechaEntrega"].Value);
+                int precio = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["precio"].Value);
+                int numeroDeTransferencia = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["numeroDeTransferencia"].Value);
+
+                var columnaMetodoPago = dataGridView1.SelectedRows[0].Cells["metodoPago"].Value;
+                if(columnaMetodoPago != null)
+                {
+                    int intValue;
+                    if (int.TryParse(columnaMetodoPago.ToString(), out intValue))
+                    {
+                        metodoPago = (Epago)intValue;
+                    }
+                }
+
+                var columnatipoEntrega = dataGridView1.SelectedRows[0].Cells["tipoEntrega"].Value;
+                if (columnatipoEntrega != null)
+                {
+                    int intValue;
+                    if (int.TryParse(columnatipoEntrega.ToString(), out intValue))
+                    {
+                        tipoEntrega = (EtipoEntrega)intValue;
+                    }
+                }
+
+                Componente componenteBorrar = new Componente
+                {
+                    id = id,
+                    nombre = nombre,
+                    fechaEntrega = fechaEntrega,
+                    precio = precio,
+                    numeroDeTransferencia = numeroDeTransferencia,
+                };
+
+                DialogResult result = MessageBox.Show($"Estas seguro que quieres eliminar este pedido para {nombre} de la lista?", "Aviso!!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    Administracion.EliminarComponente(componenteBorrar.id);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = Administracion.componentes;
+                }
+                else
+                {
+                    MessageBox.Show("Eliminacion cancelada", "Aviso!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                
+            }
+
+        }
+
+        private void btnCerrarSesion_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AlmacenDeComponentes_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Administracion.componentes;
+
+            usuario = Usuario.DesealizarUsuariosJson();
+
+            label5.Text = usuario[0].nombre;
+            
+        }
+    }
+}
---
+++ testFormParcial/modificarComponente.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace testFormParcial
+{
+    partial class modificarComponente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Click += button2_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DataSource = new EmarcaDisco[]
+    {
+    EmarcaDisco.Kingston,
+    EmarcaDisco.Seagate,
+    EmarcaDisco.Sandisk
+    };
+            //comboBox2.Items.AddRange(new object[] { EmarcaDisco.Kingston, EmarcaDisco.Seagate, EmarcaDisco.Sandisk });
+            // 
+            // comboBox3
+            // 
+            comboBox3.DataSource = new Ecolor[]
+    {
+    Ecolor.Blanco,
+    Ecolor.Negro,
+    Ecolor.Plateado
+    };
+            //comboBox3.Items.AddRange(new object[] { Ecolor.Blanco, Ecolor.Negro, Ecolor.Plateado });
+            // 
+            // comboBox4
+            // 
+            comboBox4.DataSource = new EmarcaMemoria[]
+    {
+    EmarcaMemoria.ADATA,
+    EmarcaMemoria.Corsair,
+    EmarcaMemoria.Crucial,
+    EmarcaMemoria.Gskill,
+    EmarcaMemoria.Kingston
+    };
+            //comboBox4.Items.AddRange(new object[] { EmarcaMemoria.ADATA, EmarcaMemoria.Corsair, EmarcaMemoria.Crucial, EmarcaMemoria.Gskill, EmarcaMemoria.Kingston });
+            // 
+            // comboBox5
+            // 
+            comboBox5.DataSource = new EcolorMemoria[]
+    {
+    EcolorMemoria.Verde,
+    EcolorMemoria.Negro
+    };
+            //comboBox5.Items.AddRange(new object[] { EcolorMemoria.Verde, EcolorMemoria.Negro });
+            // 
+            // comboBox6
+            // 
+            comboBox6.DataSource = new EmarcaTarjeta[]
+    {
+    EmarcaTarjeta.AMD,
+    EmarcaTarjeta.ASUS,
+    EmarcaTarjeta.NVIDIA,
+    EmarcaTarjeta.Intel,
+    EmarcaTarjeta.MSI,
+    EmarcaTarjeta.Sapphire,
+    EmarcaTarjeta.Zotac,
+    EmarcaTarjeta.GigabyteTecnology
+    };
+            //comboBox6.Items.AddRange(new object[] { EmarcaTarjeta.AMD, EmarcaTarjeta.ASUS, EmarcaTarjeta.NVIDIA, EmarcaTarjeta.Intel, EmarcaTarjeta.MSI, EmarcaTarjeta.Sapphire, EmarcaTarjeta.Zotac, EmarcaTarjeta.GigabyteTecnology });
+            // 
+            // comboBox7
+            // 
+            comboBox7.DataSource = new EserieNvidia[]
+    {
+    EserieNvidia.RTX,
+    EserieNvidia.GTX
+    };
+            //comboBox7.Items.AddRange(new object[] { EserieNvidia.RTX, EserieNvidia.GTX });
+            // 
+            // comboBox8
+            // 
+            comboBox8.DataSource = new EtipoEntrega[]
+    {
+    EtipoEntrega.Envio,
+    EtipoEntrega.Local
+    };
+            //comboBox8.Items.AddRange(new object[] { EtipoEntrega.Envio, EtipoEntrega.Local });
+            // 
+            // comboBox9
+            // 
+            comboBox9.DataSource = new Epago[]
+    {
+    Epago.Efectivo,
+    Epago.TarjetaDeCredito,
+    Epago.TarjetaDeDebito,
+    Epago.Aplicacion
+    };
+            //comboBox9.Items.AddRange(new object[] { Epago.Efectivo, Epago.TarjetaDeCredito, Epago.TarjetaDeDebito, Epago.Aplicacion });
+            // 
+            // modificarComponente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(865, 386);
+            Name = "modificarComponente";
+            Text = "modificarComponente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+    }
+}
