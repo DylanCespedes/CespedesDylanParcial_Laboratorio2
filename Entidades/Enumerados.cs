0cf8223a3ef159f94d65830a95f497bd74c3bead
Dylan Cespedes <Dylancesp001@hotmail.com>
2024-06-17 15:02:01 -0300

    Commit actualizado para el form de AlmacenDeComponentes(ahora al cerrar sesion se cierra la aplicacion)

---
+++ Entidades/Enumerados.cs
@@ -0,0 +1,75 @@
+﻿public enum EmarcaDisco
+{
+    Kingston,
+    Seagate,
+    Sandisk
+}
+
+public enum Ecolor
+{
+    Blanco,
+    Negro,
+    Plateado
+}
+
+//Componentes
+
+public enum Epago
+{
+    Efectivo,
+    TarjetaDeCredito,
+    TarjetaDeDebito,
+    Aplicacion
+}
+
+public enum EtipoEntrega
+{
+    Envio,
+    Local
+}
+
+//Tarjeta Grafica
+
+public enum EmarcaTarjeta
+{
+    AMD,
+    ASUS,
+    NVIDIA,
+    Intel,
+    MSI,
+    Sapphire,
+    Zotac,
+    GigabyteTecnology
+}
+
+public enum EserieNvidia
+{
+    RTX,
+    GTX
+}
+
+//Memoria RAM
+
+public enum EcolorMemoria
+{
+    Verde,
+    Negro
+}
+
+public enum EmarcaMemoria
+{
+    ADATA,
+    Corsair,
+    Crucial,
+    Gskill,
+    Kingston
+}
+
+//Windows form
+public enum Ecomponentes
+{
+    DiscoDuro,
+    MemoriaRAM,
+    TarjetaGrafica
+}
+
---
+++ testConsola/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+
+namespace testConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Usuario> lista = Usuario.DesealizarUsuariosJson();
+
+            foreach (Usuario usuario in lista)
+            {
+                Console.WriteLine(usuario);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
