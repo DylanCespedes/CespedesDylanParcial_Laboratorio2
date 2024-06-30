76d6444472ade77c7efa3db263143ce3ac94e686
Dylan Cespedes <Dylancesp001@hotmail.com>
2024-06-30 18:20:17 -0300

    Commit modificado para utilizar las clases hijas en los forms

--- Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3355a846b22a33c573f0521788cdd00252ae47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- testFormParcial/obj/Debug/net6.0-windows/testFormParcial.AssemblyInfo.cs
+++ testFormParcial/obj/Debug/net6.0-windows/testFormParcial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testFormParcial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3355a846b22a33c573f0521788cdd00252ae47")]
 [assembly: System.Reflection.AssemblyProductAttribute("testFormParcial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testFormParcial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
