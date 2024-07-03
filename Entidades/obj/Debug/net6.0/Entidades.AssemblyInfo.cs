ce782385fd99f9d9ce4ab1d555abcdae8e6995de
Dylan Cespedes <dylancesp001@gmail.com>
2024-07-03 00:58:27 -0300

    Commit final para el 2do parcial

--- Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266ef2636af657f1746c1c89146da11ae652a232")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
